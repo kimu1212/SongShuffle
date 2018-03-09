33d09a7773adbc3d6542f51500d057968f9fc587
木村拓也 <kimuratakuya@Mac.local>
2018-03-09 17:30:16 +0900

    indexをidに

--- src/main/java/com/songproject/song/model/Song.java
+++ src/main/java/com/songproject/song/model/Song.java
@@ -2,21 +2,21 @@ package com.songproject.song.model;
 
 public class Song {
     private String name = null;
-    private int index = 0;
+    private int id = 0;
 
     public String getName(){
         return name;
     }
 
-    public int getIndex(){
-        return index;
+    public int getId(){
+        return id;
     }
 
     public void setName(String name){
         this.name = name;
     }
 
-    public void setIndex(int index){
-        this.index = index;
+    public void setId(int id){
+        this.id = id;
     }
 }
