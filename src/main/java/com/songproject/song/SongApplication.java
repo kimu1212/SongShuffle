f4f3299928f450f0968087e35f5cd7e43673da08
木村拓也 <kimuratakuya@Mac.local>
2018-03-09 15:44:49 +0900

    first commit

---
+++ src/main/java/com/songproject/song/SongApplication.java
@@ -0,0 +1,20 @@
+package com.songproject.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SongApplication {
+
+	@RequestMapping
+	String hello(){
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongApplication.class, args);
+	}
+}
