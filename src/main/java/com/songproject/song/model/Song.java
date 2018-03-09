c03f838dac2d2e6a483ee10c340c80dc2e04a848
木村拓也 <kimuratakuya@Mac.local>
2018-03-09 17:32:53 +0900

    statusの追加

--- src/main/java/com/songproject/song/model/Song.java
+++ src/main/java/com/songproject/song/model/Song.java
@@ -3,6 +3,7 @@ package com.songproject.song.model;
 public class Song {
     private String name = null;
     private int id = 0;
+    private int status = 0;
 
     public String getName(){
         return name;
@@ -12,11 +13,19 @@ public class Song {
         return id;
     }
 
+    public int getStatus(){
+        return status;
+    }
+
     public void setName(String name){
         this.name = name;
     }
 
     public void setId(int id){
         this.id = id;
+    }
+
+    public void setStatus(int status){
+        this.status = status;
     }
 }
